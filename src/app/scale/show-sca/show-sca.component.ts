ef5d13694b2103386cc2b70aaa196f7ff9fcb74f
Tinyiko <tinyiko13@yahoo.com>
2020-11-08 23:12:55 +0200

    third commit

---
+++ src/app/scale/show-sca/show-sca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-sca',
+  templateUrl: './show-sca.component.html',
+  styleUrls: ['./show-sca.component.css']
+})
+export class ShowScaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+//import HttPClient and Observable Modules
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor() { }
+}
